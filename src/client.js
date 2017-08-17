a104a42d2ecc3713ca6df482446214656d889d25
Richard <computerichy@live.co.uk>
2017-08-17 16:11:30 +0100

    Remove sha256
    Unnecessary double hashing

--- src/client.js
+++ src/client.js
@@ -686,7 +686,7 @@
     getWebGlPrint: function() {
  
 
-
+ 
 
 
 
@@ -741,98 +741,94 @@ function webgl(){
   ctx.readPixels(0, 0, width, height, ctx.RGBA, ctx.UNSIGNED_BYTE, n);
   m = JSON.stringify(n).replace(/,?"[0-9]+":/g, "");
 
-  var hash = sha256(m);
+  return m;
+
 
 
 }
 
-var sha256 = (function(){
+// var sha256 = (function(){
 
-  // Eratosthenes seive to find primes up to 311 for magic constants. This is why SHA256 is better than SHA1
-  var i = 1,
-      j,
-      K = [],
-      H = [];
+//   // Eratosthenes seive to find primes up to 311 for magic constants. This is why SHA256 is better than SHA1
+//   var i = 1,
+//       j,
+//       K = [],
+//       H = [];
 
-  while(++i < 18){
-    for(j = i * i; j < 312; j += i){
-      K[j] = 1;
-    }
-  }
+//   while(++i < 18){
+//     for(j = i * i; j < 312; j += i){
+//       K[j] = 1;
+//     }
+//   }
 
-  function x(num, root){
-    return (Math.pow(num, 1 / root) % 1) * 4294967296|0;
-  }
+//   function x(num, root){
+//     return (Math.pow(num, 1 / root) % 1) * 4294967296|0;
+//   }
 
-  for(i = 1, j = 0; i < 313; ){
-    if(!K[++i]){
-      H[j] = x(i,2);
-      K[j++] = x(i,3);
-    }
-  }
+//   for(i = 1, j = 0; i < 313; ){
+//     if(!K[++i]){
+//       H[j] = x(i,2);
+//       K[j++] = x(i,3);
+//     }
+//   }
 
-  function S (X, n) { return ( X >>> n ) | (X << (32 - n)); }
+//   function S (X, n) { return ( X >>> n ) | (X << (32 - n)); }
 
-  function SHA256(b){
-    var HASH = H.slice(i = 0),
-        s = unescape(encodeURI(b)), /* encode as utf8 */
-        W = [],
-        l = s.length,
-        m = [],
-        a, y, z;
-    for(; i < l; ) m[i >> 2] |= (s.charCodeAt(i) & 0xff) << 8 * (3 - i++ % 4);
+//   function SHA256(b){
+//     var HASH = H.slice(i = 0),
+//         s = unescape(encodeURI(b)), /* encode as utf8 */
+//         W = [],
+//         l = s.length,
+//         m = [],
+//         a, y, z;
+//     for(; i < l; ) m[i >> 2] |= (s.charCodeAt(i) & 0xff) << 8 * (3 - i++ % 4);
 
-    l *= 8;
+//     l *= 8;
 
-    m[l >> 5] |= 0x80 << (24 - l % 32);
-    m[z = (l + 64 >> 5) | 15] = l;
+//     m[l >> 5] |= 0x80 << (24 - l % 32);
+//     m[z = (l + 64 >> 5) | 15] = l;
 
-    for(i = 0; i < z; i += 16){
-      a = HASH.slice(j = 0, 8);
+//     for(i = 0; i < z; i += 16){
+//       a = HASH.slice(j = 0, 8);
 
-      for(; j < 64; a[4] += y){
-        if(j < 16){
-          W[j] = m[j + i];
-        }else{
-          W[j] =
-            (S(y = W[j - 2], 17) ^ S(y, 19) ^ (y >>> 10)) +
-            (W[j - 7]|0) +
-            (S(y = W[j - 15], 7) ^ S(y, 18) ^ (y >>> 3)) +
-            (W[j - 16]|0);
-        }
+//       for(; j < 64; a[4] += y){
+//         if(j < 16){
+//           W[j] = m[j + i];
+//         }else{
+//           W[j] =
+//             (S(y = W[j - 2], 17) ^ S(y, 19) ^ (y >>> 10)) +
+//             (W[j - 7]|0) +
+//             (S(y = W[j - 15], 7) ^ S(y, 18) ^ (y >>> 3)) +
+//             (W[j - 16]|0);
+//         }
 
-        a.unshift(
-          (
-            y = (
-              a.pop() +
-              (S(b = a[4], 6) ^ S(b, 11) ^ S(b, 25)) +
-              (((b & a[5]) ^ ((~b) & a[6])) + K[j])|0
-            ) +
-            (W[j++]|0)
-          ) +
-          (S(l = a[0], 2) ^ S(l, 13) ^ S(l, 22)) +
-          ((l & a[1]) ^ (a[1] & a[2]) ^ (a[2] & l))
-        );
-      }
+//         a.unshift(
+//           (
+//             y = (
+//               a.pop() +
+//               (S(b = a[4], 6) ^ S(b, 11) ^ S(b, 25)) +
+//               (((b & a[5]) ^ ((~b) & a[6])) + K[j])|0
+//             ) +
+//             (W[j++]|0)
+//           ) +
+//           (S(l = a[0], 2) ^ S(l, 13) ^ S(l, 22)) +
+//           ((l & a[1]) ^ (a[1] & a[2]) ^ (a[2] & l))
+//         );
+//       }
 
-      for(j = 8; j--; ) HASH[j] = a[j] + HASH[j];
-    }
+//       for(j = 8; j--; ) HASH[j] = a[j] + HASH[j];
+//     }
 
-    for(s = ''; j < 63; ) s += ((HASH[++j >> 3] >> 4 * (7 - j % 8)) & 15).toString(16);
+//     for(s = ''; j < 63; ) s += ((HASH[++j >> 3] >> 4 * (7 - j % 8)) & 15).toString(16);
 
-    return s;
-  }
+//     return s;
+//   }
 
-  return SHA256;
-})();
-
-
-var webgl_hash = webgl();   
-console.log(webgl_hash);
-
-return webgl_hash;
+//   return SHA256;
+// })();
 
 
+return webgl();
 
 
 
